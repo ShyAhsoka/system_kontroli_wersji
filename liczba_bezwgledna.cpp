7b9925527b014685c5e5db0e350da32f488e8ab0
ShyAhsoka <amelzt66@gmail.com>
2022-02-16 15:17:50 +0100

    dodanie opisu działania codu
    dodanie opisow w "if" tlumczacej co dokladnie dzieje sie w danym kawałku kodu

--- liczba_bezwgledna.cpp
+++ liczba_bezwgledna.cpp
@@ -9,10 +9,12 @@ int main()
     cout << "|" << n << "| = ";
     if (n >= 0)
     {
+        // wyswietla liczbe bezwgledna
          cout << n << endl;
     }
     else
     {
+        //dodaje -1 jesli liczba jest ujemna aby otrzymac bezwgledna liczbe
         cout << n * (-1) << endl;
     }
 }
